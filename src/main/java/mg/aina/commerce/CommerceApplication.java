3ad9919f131576d6110c7923dda7b071d013b423
aina <herimahefasoaaina14@gmail.com>
2026-08-20 15:00:46 +0300

    add dependance bootstrap (offligne)

--- src/main/java/mg/aina/commerce/CommerceApplication.java
+++ src/main/java/mg/aina/commerce/CommerceApplication.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
